9639c3f8b77522b7676a250aac508925783fd4d9
Jihee Choo <jiheechoo01@berkeley.edu>
2020-08-03 13:11:15 -0700

    finished lab 20 git add .!

--- lab20/UnionFindTest.java
+++ lab20/UnionFindTest.java
@@ -27,27 +27,4 @@ public class UnionFindTest {
 //        System.out.println(set.sizeOf(0));
         System.out.println(set.find(6));
     }
-
-    @Test
-    public void test2() {
-        UnionFind uf = new UnionFind(5);
-        uf.union(0, 1);
-        uf.union(3, 2);
-        uf.union(2, 1);
-        uf.find(3);
-        assertEquals(1, uf.parent(3));
-        uf.union(0, 4);
-        uf.union(4, 0);
-        assertEquals(5, uf.sizeOf(2));
-    }
-
-    @Test
-    public void test3() {
-        UnionFind uf = new UnionFind(2);
-        uf.union(0, 1);
-        System.out.println(uf.parent(1));
-        System.out.println(uf.parent(0));
-    }
-
-
 }
